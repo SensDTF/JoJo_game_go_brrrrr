d94d1ee82cbbf728a4de645c62c541e3bb7dff29
dsan22 <jankovicdusan00@gmail.com>
2021-10-22 16:51:19 +0200

    Stand Folow Update better roatation and cd

--- Jojo igrica/Assets/StandScript.cs
+++ Jojo igrica/Assets/StandScript.cs
@@ -6,33 +6,65 @@ public class StandScript : MonoBehaviour
 {
     public Transform StandPlace, AttackPlace, ReturnPlace;
 
+    float AttackCd = 3f;
+    bool Attacked;
+
     StandFolow folowScript;
+
+    RotateTo roatateScript;
     
     void Start()
     {
+        Attacked = false;
         folowScript = GetComponent<StandFolow>();
-        folowScript.Folow(StandPlace);
+        roatateScript= transform.GetChild(0).gameObject.GetComponent<RotateTo>();
     }
 
     
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
+        if (AttackCd > 0) {
+            AttackCd -= Time.deltaTime;
+        }
+        if (AttackCd < 0) {
+            AttackCd = 0;
+
+        }
+
+
+        if (Input.GetMouseButtonDown(0)&& AttackCd==0) {
             Attack();
+            
         }
         if (Input.GetMouseButtonUp(0))
         {
             StopAttack();
+            
         }
 
     }
 
     public void Attack() {
         folowScript.Folow(AttackPlace);
+        roatateScript.ChangeTarget(AttackPlace.GetChild(0).transform);
+        folowScript.SetMs(15);
+
+        Attacked = true;
+
     }
     public void StopAttack()
     {
         folowScript.Folow(StandPlace);
+        folowScript.SetMs(14);
+        Invoke("StartRotate", 1);
+        if (Attacked)
+        {
+            AttackCd = 3f;
+            Attacked = false;
+        }
+    }
+    public void StartRotate() {
+        roatateScript.ChangeTarget(ReturnPlace);
     }
 
 }
